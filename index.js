89549615a455b3532d13bf00abce02899055473b
John Ratana <jratana1@gmail.com>
2021-01-26 09:29:39 -0500

    commit tree 21173c31a0b3d46e092167fdeb06001d47ff78a2

---
+++ index.js
@@ -0,0 +1,40 @@
+function wakeDog(dogName, dogBreed) {
+    let string = `Wake ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  function leashDog(dogName, dogBreed) {
+    let string = `Leash ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  function walkToPark(dogName, dogBreed) {
+    let string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  function throwFrisbee(dogName, dogBreed) {
+    let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  function walkHome(dogName, dogBreed) {
+    let string = `Walk home with ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  function unleashDog(dogName, dogBreed) {
+    let string = `Unleash ${dogName} the ${dogBreed}`;
+    return string
+  }
+
+  const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+  function exerciseDog(dogName, dogBreed){
+    let answer = []
+    for (let i = 0; i < routine.length; i++) {
+        answer.push(routine[i](dogName,dogBreed))
+    }
+    
+      return answer;
+  }
